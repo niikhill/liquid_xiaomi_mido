c99c88050d8d135c6692479d791d35ae75cda1d8
sathish2026 <txt2sathish@gmail.com>
2018-01-06 12:41:54 +0100

    Mido: Enable compilation to hidl.manager

--- device.mk
+++ device.mk
@@ -213,7 +213,9 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
 
 # IPA Manager
 PRODUCT_PACKAGES += \
